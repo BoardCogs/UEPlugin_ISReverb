c4743d2f552f719c36408f9ca1f203265e623315
BoardCogs <gianmaria.forte2002@gmail.com>
2025-11-08 19:59:55 +0100

    Add code to project

---
+++ Source/UEPlugin_ISReverb/UEPlugin_ISReverb.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UEPlugin_ISReverb.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UEPlugin_ISReverb, "UEPlugin_ISReverb" );
