a970142df6fabef2a2aa54e90da42ca54c4f193c
BoardCogs <gianmaria.forte2002@gmail.com>
2025-11-14 11:52:51 +0100

    Add ReflectorEdge class

---
+++ Source/UEPlugin_ISReverb/ReflectorEdge.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ReflectorEdge.h"
+
+ReflectorEdge::ReflectorEdge()
+{
+}
+
+ReflectorEdge::~ReflectorEdge()
+{
+}
