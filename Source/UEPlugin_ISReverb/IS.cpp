027de3e4afa15fc8959b3532dac7d2b02370190e
BoardCogs <gianmaria.forte2002@gmail.com>
2025-11-18 19:33:17 +0100

    Define IS class

--- Source/UEPlugin_ISReverb/IS.cpp
+++ Source/UEPlugin_ISReverb/IS.cpp
@@ -1,8 +1,27 @@
 #include "IS.h"
 
-IS::IS()
+IS::IS(int i, int o, int p, int s, ISBeamProjection beam, bool v)
 {
+	Index = i;
+	Order = o;
+	Parent = p;
+	Surface = s;
+	BeamPoints = beam;
+	Valid = v;
+	HasPath = true;
+	Path = TArray<FVector3f>();
 }
+
+
+
+// Sets the path followed by the sound ray reflection for this IS, if present
+void IS::SetPath(bool b, TArray<FVector3f> p)
+{
+	HasPath = b;
+	Path = p;
+}
+
+
 
 IS::~IS()
 {
--- Source/UEPlugin_ISReverb/IS.h
+++ Source/UEPlugin_ISReverb/IS.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ISBeamProjection.h"
 
 /**
  * An Image Source, representing a possible reflection path from sound source to listener along a specific set of surfaces.
@@ -8,6 +9,36 @@
 class UEPLUGIN_ISREVERB_API IS
 {
 public:
-	IS();
+	IS(int i, int o, int p, int s, ISBeamProjection beam, bool v = true);
+
+	// The index of this Image Source in its ISTree
+	int Index;
+
+	// The order of this IS
+	int Order;
+
+	// The index of this Image Source's parent
+	int Parent;
+
+	// The index of this Image Source's reflector
+	int Surface;
+
+	// The position of the Image Source
+	FVector3f Position = FVector3f::Zero();
+
+	// The points and edges resulting from beam tracing on this surface's reflector from its parent IS
+	ISBeamProjection BeamPoints = ISBeamProjection(TArray<FVector3f>(), TArray<ReflectorEdge>());
+
+	// If false, the IS should have been removed
+	bool Valid;
+
+	// If true, the IS has a reflection path that reaches the listener
+	bool HasPath = true;
+
+	// The reflection path followed by sound to reach the listener
+	TArray<FVector3f> Path;
+	
+	void SetPath(bool b, TArray<FVector3f> p);
+	
 	~IS();
 };
